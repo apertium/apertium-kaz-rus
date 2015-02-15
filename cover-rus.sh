0ecba03ce5d66978deb821be7ef45b03b45cda76
Aida Sundetova <sun27aida@gmail.com>
2015-02-15 14:47:46 +0000

    checking coverage

---
+++ cover-rus.sh
@@ -0,0 +1,13 @@
+DIX=/home/ubuntu/Source/apertium-rus/apertium-rus.rus.dix
+BIN=/home/ubuntu/Source/apertium-kaz-rus/kaz-rus.automorf.bin
+cat /home/ubuntu/Source/apertium-kaz-rus/texts/rus.txt | cut -f2 | grep -v '>(' | sed 's/&lt;/</g' | sed 's/&gt;/>/g' | apertium-destxt | lt-proc -w $BIN | apertium-retxt | sed 's/\$\W*\^/$\n^/g' > /tmp/rus.coverage.txt
+
+EDICT=`cat $DIX | grep '<e lm' | wc -l`;
+EPAR=`cat $DIX | grep '<pardef n' | wc -l`;
+TOTAL=`cat /tmp/rus.coverage.txt | wc -l`
+KNOWN=`cat /tmp/rus.coverage.txt | grep -v '*' | wc -l`
+COV=`calc $KNOWN / $TOTAL`;
+DATE=`date`;
+
+echo -e $DATE"\t"$EPAR":"$EDICT"\t"$KNOWN"/"$TOTAL"\t"$COV >> history.log
+tail -1 history.log
