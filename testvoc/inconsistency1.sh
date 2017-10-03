46e33164ec21fbf00bfb7d9edc2ab91300fd9293
Balzhan Abduali <balzhan_5696@mail.ru>
2017-10-03 16:52:09 +0000

    some new rules Balzhan

--- testvoc/inconsistency1.sh
+++ testvoc/inconsistency1.sh
@@ -9,14 +9,14 @@ if [[ $DIR = "rus-kaz" ]]; then
 #lt-expand ../../apertium-eng-kaz.eng.dix | grep -v -e '<compound-' -e 'DUE_TO_LT_PROC_HANG' | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | $SED 's/:>:/%/g' | $SED 's/:/%/g' | cut -f2 -d'%' |  $SED 's/^/^/g' | $SED 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
 
 # Run the bilingual dictionary before to make sure we are only checking things we have.
-lt-expand ~/sources/apertium-rus/apertium-rus.rus.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | $SED 's/:>:/%/g' | $SED 's/:/%/g' | cut -f2 -d'%' |  $SED 's/^/^/g' | $SED 's/$/$ ^.<sent>$/g' | apertium-pretransfer | lt-proc -b ~/sources/apertium-kaz-rus/rus-kaz.autobil.bin | grep -v '/@' | cut -f1 -d'/' | $SED 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |\
+lt-expand ~/source-kaz-rus/apertium-rus/apertium-rus.rus.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | $SED 's/:>:/%/g' | $SED 's/:/%/g' | cut -f2 -d'%' |  $SED 's/^/^/g' | $SED 's/$/$ ^.<sent>$/g' | apertium-pretransfer | lt-proc -b ~/source-kaz-rus/apertium-kaz-rus/rus-kaz.autobil.bin | grep -v '/@' | cut -f1 -d'/' | $SED 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |\
         apertium-pretransfer|\
-	lt-proc -b ~/sources/apertium-kaz-rus/rus-kaz.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |\
-        apertium-transfer -b ~/sources/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t1x  ~/sources/apertium-kaz-rus/rus-kaz.t1x.bin |\
-        apertium-interchunk ~/sources/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t2x  ~/sources/apertium-kaz-rus/rus-kaz.t2x.bin |\
-        apertium-postchunk ~/sources/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t3x ~/sources/apertium-kaz-rus/rus-kaz.t3x.bin |\
-        apertium-transfer -n ~/sources/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t4x  ~/sources/apertium-kaz-rus/rus-kaz.t4x.bin | tee $TMPDIR/$DIR.tmp_testvoc3.txt |\
-        hfst-proc -d ~/sources/apertium-kaz-rus/rus-kaz.autogen.hfst > $TMPDIR/$DIR.tmp_testvoc4.txt
+	lt-proc -b ~/source-kaz-rus/apertium-kaz-rus/rus-kaz.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |\
+        apertium-transfer -b ~/source-kaz-rus/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t1x  ~/source-kaz-rus/apertium-kaz-rus/rus-kaz.t1x.bin |\
+        apertium-interchunk ~/source-kaz-rus/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t2x  ~/source-kaz-rus/apertium-kaz-rus/rus-kaz.t2x.bin |\
+        apertium-postchunk ~/source-kaz-rus/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t3x ~/source-kaz-rus/apertium-kaz-rus/rus-kaz.t3x.bin |\
+        apertium-transfer -n ~/source-kaz-rus/apertium-kaz-rus/apertium-kaz-rus.rus-kaz.t4x  ~/source-kaz-rus/apertium-kaz-rus/rus-kaz.t4x.bin | tee $TMPDIR/$DIR.tmp_testvoc3.txt |\
+        hfst-proc -d ~/source-kaz-rus/apertium-kaz-rus/rus-kaz.autogen.hfst > $TMPDIR/$DIR.tmp_testvoc4.txt
 paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt| $SED 's/\^.<sent>\$//g' | $SED 's/_/   --------->  /g'
 
 
